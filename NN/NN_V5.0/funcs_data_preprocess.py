9da56ac9050e81ce9323358edf82304ac2db49a1
Logan-Kelsch <7logankelsch5@gmail.com>
2024-11-18 00:15:55 -0500

    good fetch/pull commit, read desc
    working func file, working PCA, not working test set. try new model, try ToS scrape code, try custom val set

--- NN/NN_V5.0/funcs_data_preprocess.py
+++ NN/NN_V5.0/funcs_data_preprocess.py
@@ -7,9 +7,14 @@
 
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+#import matplotlib as plt
 
 #setting data for LSTM
-def reformat_to_lstm(X, time_steps=5):
+def reformat_to_lstm(X, time_steps):
     X_lstm, y_lstm = [], []
     
     for i in range(time_steps, len(X)):
@@ -28,7 +33,7 @@ def reformat_to_lstm(X, time_steps=5):
         REAPPENDED TO THE FUNCTION PROVIDED SET BEFORE THEY ARE CALLED.
 '''
 
-def remove_zero_mo_samples(X, y, Xfeatures, timeSteps=5):
+def remove_zero_mo_samples(X, y, timeSteps):
     # Get the 'MO' column (index 34 for 0-based indexing) for all time steps and samples
     non_zero_indices = (X[:, timeSteps-1, X.shape[2]-1] >= 0)
     # Filter X and y using these indices
@@ -37,7 +42,7 @@ def remove_zero_mo_samples(X, y, Xfeatures, timeSteps=5):
     print(f'Remaining Sample Count - remove_zero_mo_samples:\n\t{len(X_filtered)}')
     return X_filtered, y_filtered
 
-def remove_extra_filter(X, y, Xfeatures, timeSteps, timeStart, timeStop):
+def remove_extra_filter(X, y, timeSteps, timeStart, timeStop):
     indices = (X[:, timeSteps-1, X.shape[2]-3] >= timeStart)#-3 is ToD, this value is 9:30am
     X = X[indices]
     y = y[indices]
@@ -66,3 +71,126 @@ def target_setter(data, testFor):
         case 'r60':
             data = data.drop(columns=['r1','r2','r3','r5','r10','r15','r30'])
     return data
+
+
+def normalize_from_tt_split(X_sctran, X_scfit, test_size):
+    fit_cutter = int(len(X_scfit)*(1-test_size))
+    #scale all data to that of the projected training set datapoints
+    X_fit = X_scfit[:fit_cutter]
+
+    scaler1 = StandardScaler()
+    scaler2 = RobustScaler()
+    scaler3 = MinMaxScaler(feature_range=(-1,1))
+    scaler1.fit(X_fit)
+    return scaler1.transform(X_sctran)
+
+#this function will need to be re-evaluated if 
+#various test_sizes are used around different models
+def reform_with_PCA_isolated(X_pcatran, X_pcafit, test_size, num_isol_feats, comps_PCA):
+    fit_cutter_fit = int(len(X_pcafit)*(1-test_size))
+    X_main_features_fit = X_pcafit[:, :-num_isol_feats]
+    X_fit = X_main_features_fit[:fit_cutter_fit]#update fit values for PCA
+
+    X_main_features = X_pcatran[:, :-num_isol_feats]
+    X_time_features = X_pcatran[:, -num_isol_feats:]
+    
+
+    pca = PCA()
+
+    pca = PCA(n_components = comps_PCA)
+    pca.fit(X_fit)
+    X_main_features = pca.transform(X_main_features)
+
+    return np.hstack((X_main_features, X_time_features))
+
+
+
+
+
+'''
+def show_all_results(used_model, history, X_test, y_test):
+    # LOSS
+    epochs = range(1, len(history.history['loss']) + 1)
+    plt.figure(figsize=(12, 6))
+    plt.plot(epochs, history.history['loss'], 'y', label='Training Loss')
+    plt.plot(epochs, history.history['val_loss'], 'r', label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['Train', 'Validation'], loc='upper right')
+    plt.show()
+    # ACCURACY
+
+    plt.plot(epochs, history.history['R2Score'], 'y', label='Training R2')
+    plt.plot(epochs, history.history['val_R2Score'], 'r', label='Validation R2')
+    plt.title('Training and Validation R2Score')
+    plt.xlabel('Epoch')
+    plt.ylabel('R2Score')
+    plt.legend(['Train', 'Validation'], loc='upper right')
+    plt.show()
+
+
+    #predicting the test set results
+    y_pred = used_model.predict(X_test) 
+
+
+    plt.scatter(y_pred, y_test, s=1)
+    plt.axis('tight')
+    plt.title('Testing Outputs')
+    plt.xlabel('y_pred')
+    plt.xlim(-.25,.25)
+    plt.ylim(-.25,.25)
+    plt.ylabel('y_test')
+    ax = plt.gca()
+    x_vals = np.array(ax.get_xlim())
+    y_vals = x_vals  # Since y = x
+    plt.plot(x_vals, y_vals, '-', color='black', label='y = x', linewidth=0.5)
+    plt.axhline(0, color='black', linewidth=0.5)
+    plt.axvline(0,color='black',linewidth=0.5)
+    plt.show()
+
+    #SCATTERPLOT #SCATTERPLOT  #SCATTERPLOT  #SCATTERPLOT  #SCATTERPLOT  #SCATTERPLOT  #SCATTERPLOT  #SCATTERPLOT  
+    plt.scatter(y_pred, y_test, s=1)
+    plt.grid()
+    plt.axis('tight')
+    plt.title('Testing Outputs')
+    plt.xlabel('y_pred')
+    plt.xlim(-1,1)
+    plt.ylim(-1,1)
+    plt.ylabel('y_test')
+    ax = plt.gca()
+    x_vals = np.array(ax.get_xlim())
+    y_vals = x_vals  # Since y = x
+    plt.plot(x_vals, y_vals, '-', color='black', label='y = x', linewidth=0.5)
+    plt.axhline(0, color='black', linewidth=0.5)
+    plt.axvline(0,color='black',linewidth=0.5)
+    plt.show()
+    #DIRECTIONAL ACCURACY #DIRECTIONAL ACCURACY  #DIRECTIONAL ACCURACY  #DIRECTIONAL ACCURACY  #DIRECTIONAL ACCURACY  
+    tp, fp, tn, fn = 0, 0, 0, 0
+    tp5, fp5, tn5, fn5 = 0, 0, 0, 0
+    for i in range(len(y_pred)):
+        if(y_pred[i]>0):
+            if(y_test[i]>0):
+                tp+=1
+            if(y_test[i]<0):
+                fp+=1
+            if(y_pred[i]>=5):
+                if(y_test[i]>0):
+                    tp5+=1
+                if(y_test[i]<0):
+                    fp5+=1
+        if(y_pred[i]<0):
+            if(y_test[i]<0):
+                tn+=1
+            if(y_test[i]>0):
+                fn+=1
+            if(y_pred[i]<=-5):
+                if(y_test[i]<0):
+                    tn5+=1
+                if(y_test[i]>0):
+                    fn5+=1
+    directionalAccuracy = ((tp+tn)/(tp+fp+tn+fn))*10000//1/100
+    print('Directional Accuracy:\t\t',directionalAccuracy)
+    directionalAccuracy5guess = ((tp5+tn5)/(tp5+fp5+tn5+fn5))*10000//1/100
+    print('Directional Accuracy >(+/-)5:\t',directionalAccuracy5guess)
+    '''
