f5dda48f93560025e59d3281b6caa9e8301f6a44
Jairik <jairikmc@gmail.com>
2024-12-24 16:54:46 -0500

    Insignificant Shenanigans
    Looking through some of the features and plotting them on each other just to gather an idea of their relationships. Trying to find some sort of dimension which could potentially linearly separate some of the samples or something

---
+++ JJs-Shenanigans/RuntimeDataCollection-Shenanigans/APIKey1.md
@@ -0,0 +1,7 @@
+# StockData.org API Key
+
+aOWP2vRuALq9jMytMWgRzWbIZBG4J1KomISuZvF9
+
+- Currently on free plan
+- Limited intraday datacollection
+    - Advertised 100 daily API calls
---
+++ JJs-Shenanigans/RuntimeDataCollection-Shenanigans/getSDKey.py
@@ -0,0 +1,6 @@
+''' Importable class to return API Key (helper to getData.py) '''
+
+class getKey:
+    def gKey():  # get key function
+        return 'aOWP2vRuALq9jMytMWgRzWbIZBG4J1KomISuZvF9'
+        
