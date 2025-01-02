df6ecfed1969f9390f32cd40575e6066bc20313b
Logan-Kelsch <7logankelsch5@gmail.com>
2025-01-02 01:24:26 -0500

    model evaluation planned some, eval ready to be written, modelset_training working (need test)

--- RotationalEnsemble/_Model_Evaluation.py
+++ RotationalEnsemble/_Model_Evaluation.py
@@ -8,3 +8,15 @@
 #NOTE THE PARALLEL ARRAY OF X_FEAT_FIND REMEMBER THAT IT IS A 1D ARRAY, AND IT ONLY RUNS PARALLEL 
 #NOTE TO THE FEATURESPACE DIMENSION AND NOT THE SAMPLESPACE DIMENSION OR MODELSPACE DIMENSION, 
 #NOTE SO REFER ONLY TO THE INDEX OF THE FEATURE DIMENSION WHEN REFERING TO IT.
+
+'''
+	watching a podcast and coding so just going to write notes.
+
+	NOTE create a self test and independent test 3d array of performances for each model type
+    NOTE predicament: how to show the overall performance of models?
+    NOTE	-	options to show all performances
+    NOTE	-	show full averages, std deviations, high and low
+    NOTE	-	show type averages, std deviations, high and low (types: featurespace, samplespace, modelspace based)
+    NOTE	-	show feat-samp pair averages, std devs, hi n' lo (defin: for each training set)
+
+'''
