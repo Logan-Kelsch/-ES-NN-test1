3c296a0f513e1229586dff2d90ea7afdb3b64790
Logan-Kelsch <7logankelsch5@gmail.com>
2025-03-10 15:12:16 -0400

    stupid git has no easy way for me to fix my credentials on my laptop, cannot commit from vscode. halfway update of gene file.

--- zGeneticAlgorthim/0_gene.py
+++ zGeneticAlgorthim/0_gene.py
@@ -1,24 +1,77 @@
 '''
 '''
 
+import operator
+
 class Gene():
     '''
     '''
-    def __init__():
+    def __init__(
+        self,
+        patterns    :   list    =   []
+    ):
+        
+
+        self._patterns = patterns
         return
     
     #patterns - list of classes
     #index range or index list
+
+    @property
+    def patterns(self):
+        return self._patterns
+    
+    @patterns.setter
+    def patterns(self, new:any):
+        self._patterns = new
 
 
 
 class Pattern():
     '''
     '''
-    def __init__():
+    def __init__(
+        v1  :   any =   None,
+        op  :   any =   None,
+        v2  :   any =   None
+    ):
+        
         return
     #var1 - feature index
     #operator - (<, >)
     #var2 - feature index
 
     #generate random
+    def random(
+        acceptable_vals :   list    =   [],
+        acceptable_lags :   list|range  =   None
+    ):
+        assert acceptable_lags != None, \
+            f"A random pattern was requested, but {acceptable_lags} acceptable lags were entered."
+
+
+
+    @property
+    def v1(self):
+        return self._v1
+    
+    @property
+    def op(self):
+        return self._op
+    
+    @property
+    def v2(self):
+        return self._v2
+    
+    @v1.setter
+    def v1(self, new:any):
+        self._v1 = new
+
+    @op.setter
+    def op(self, new:any):
+        self._op = new
+
+    @v2.setter
+    def v2(self, new:any):
+        self._v2 = new
