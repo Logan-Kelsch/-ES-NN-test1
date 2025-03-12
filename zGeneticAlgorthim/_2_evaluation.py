4f35f785dcf13937a52345535bd8662447e51370
Logan-Kelsch <7logankelsch5@gmail.com>
2025-03-12 10:45:14 -0400

    further misc development in evaluation step of genetic algorithm

--- zGeneticAlgorthim/_2_evaluation.py
+++ zGeneticAlgorthim/_2_evaluation.py
@@ -8,11 +8,13 @@ import numpy as np
 from _0_gene import *
 
 def fitness(
-	data	:	np.ndarray	=	None,
-	returns	:	np.ndarray	=	None,
-	method	:	function	=	None,
-	sample	:	int			=	-1,
-	holdfor	:	int			=	-1
+	arr_close	:	np.ndarray	=	None,
+	arr_low		:	np.ndarray	=	None,
+	data		:	np.ndarray	=	None,
+	genes		:	list|np.ndarray	=	None,
+	method		:	function	=	None,
+	holdfor		:	int		=	-1,
+	specific_data	:	str		=	None
 ):
 	'''
 	### info: ###
@@ -21,6 +23,32 @@ def fitness(
 	returns:
 	-
 	'''
+	#function bare-bones assertions
+	assert data != None, "No data was provided to the fitness function."
+	assert genes != None, "No genes were provided to the fitness function."
+	assert method != None, "No ground-truth method was provided to the fitness function."
+	assert holdfor != -1, "No holdfor length was provided to the fitness function."
+	
+	#close and low data specific assertions and array formations
+	match(specific_data):
+		case None:
+			assert arr_close != None, \
+				"Data is not specified, but close data was not provided to the fitness function."
+			if(method is martin_ratio):
+				assert arr_low != None, \
+					"Data is not specified, martin ratio is selected, but low data was not provided to the fitness function."
+		case "form_519":
+			raise notImplementedError("MAKE INDEX VALUE")
+			some_index = -1
+			arr_close = data[:, some_index]
+			arr_low = data[:, some_index]
+
+	#boolean 2d array containing entry/or-not (0|1) for each gene
+	gene_presence = []
+
+
+	gene_presence = np.array(gene_presence)
+	
 	return 
 
 def profit_factor(
