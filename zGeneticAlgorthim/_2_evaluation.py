522304d27532fc38eddd2040095f37e2d6d172f1
Logan-Kelsch <7logankelsch5@gmail.com>
2025-03-13 14:01:24 -0400

    General statistic development in evaluate file

--- zGeneticAlgorthim/_2_evaluation.py
+++ zGeneticAlgorthim/_2_evaluation.py
@@ -5,6 +5,7 @@ This file will contain all evaluation functions - created 3/10/2025
 #DEV NOTE ensure all fitfuncs are generated parallel to data to allow for parallel analysis.
 
 import numpy as np
+from math import sqrt
 from _0_gene import *
 
 def fitness(
@@ -51,32 +52,62 @@ def fitness(
 
 	#test all samples in the set, accounting for
 	#lag allowance and hold length
-	for i in range(lag_allow, length-hold_for):
+	for i in range(length):
 		
-		i_presence = []
+		if(i < lag_allow | i > length-hold_for):
+			#want to avoid usage of these values for safe analysis
+		else:
 		
-		#check presence of each gene at each sample
-		for gene in genes:
-			matches = True
-			
-			for p in gene._patterns:
-				#if given pattern holds true
-				if(p._op(data[i-p._l1, p._v1],data[i-p._l2, p._v2])
-					pass#pattern matches
+			i_presence = []
+		
+			#check presence of each gene at each sample
+			for gene in genes:
+				matches = True
+				for p in gene._patterns:
+					#if given pattern holds true
+					if(p._op(data[i-p._l1, p._v1],data[i-p._l2, p._v2])
+						pass#pattern matches
+					else:
+						matches = False
+						break
+				#check if matches variable held
+				if(matches == False):
+					i_presence.append(0)
 				else:
-					matches = False
-					break
-		
-			#check if matches variable held
-			if(matches == False):
-				i_presence.append(0)
-			else:
-				i_presence.append(1)
+					i_presence.append(1)
 		
 		#now have a fully built sample presence
 		gene_presence.append(i_presence)
 			
 	gene_presence = np.array(gene_presence)
+	
+	returns = []
+	kelsch_index = []
+	
+	gp_len = len(gene_presence)
+	
+	#now going to take gene presence and define
+	#returns and custom index values
+	for i in range(length):
+		
+		if(i < lag_allow | i > length-hold_for):
+			#want to avoid usage of these values for safe analysis
+		else:
+		
+			#calculate returns
+			ret_local = (arr_close[i+hold_for]-arr_close[i]
+			returns.append(gene_presence[i]*ret_local)
+			
+			#calculate index values here if desired
+			ki_local = 0
+			entry_price = arr_close[i]
+			for c in range(1,hold_for+1):
+				ki_local+=((entry_price-arr_low[i+c])**2)
+			ki_local = sqrt(ki_local/hold_for)
+			
+			kelsch_ratio_local = ret_local / ki_local
+			
+			kelsch_index.append(gene_presence[i]*(kelsch_ratio_local))
 	
 	return 
 
