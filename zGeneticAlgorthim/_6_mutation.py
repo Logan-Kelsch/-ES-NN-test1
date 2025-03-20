b5af9f9c69bffb4dff9a5e63ac59d0a8ae158de0
Logan-Kelsch <7logankelsch5@gmail.com>
2025-03-20 18:58:06 -0400

    Update _6_mutation.py file from IOS

--- zGeneticAlgorthim/_6_mutation.py
+++ zGeneticAlgorthim/_6_mutation.py
@@ -2,3 +2,31 @@
 
 
 #replace entire pattern with new pattern
+
+import random
+
+def mutation_round(
+    shuffled_population:list=None,
+    partial_mutation_prob:float=0.025,
+    pattern_mutation_prob:float=0.025
+):
+
+    
+
+    return
+
+#this function generates boolean, true percent% of the time
+def chance(
+    percent:float=0.5
+):
+
+    #if percent comes in as integer
+    if(percent>1):
+        percent/=100
+
+    #if chance falls within percent window
+    if(random.random()<percent):
+        return True
+        
+    else:
+        return False
