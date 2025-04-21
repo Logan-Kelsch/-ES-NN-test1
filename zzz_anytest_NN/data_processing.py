f0c2b0d14e10f5852d4e7d8a51863472f877546b
Logan-Kelsch <7logankelsch5@gmail.com>
2025-04-21 13:39:24 -0400

    random stuff actually no utility added

--- zzz_anytest_NN/data_processing.py
+++ zzz_anytest_NN/data_processing.py
@@ -17,3 +17,19 @@ def make_prll_trgt(data, index, offset):
     
     trgt = np.roll(data[:, index], -offset)
     return data[:-offset], trgt[:-offset]
+
+
+def reformat_to_lstm(X, y, time_steps):
+    X_lstm = []
+    
+    for i in range(time_steps, len(X)):
+        # Collect previous time_steps rows for X
+        X_lstm.append(X[i-time_steps:i])  
+        # The corresponding y value for the last time step in the sequence
+    
+    X_lstm = np.array(X_lstm)
+
+    y_lstm = y[time_steps:]
+    y_lstm = np.array(y_lstm)
+    
+    return X_lstm, y_lstm
